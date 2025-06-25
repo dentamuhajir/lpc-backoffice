048d5ef139e4a179f40e3ee59c7ab880482b4872
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-25 16:12:38 +0700

    Add dashboard children

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,19 +12,9 @@ export default function DashboardLayout({
             <Sidebar/>
             <div className="layout-main-container">
                 <div className="layout-main">
-
-                    <div className="grid grid-cols-4 gap-4 mb-5">
-                        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
-                        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
-                        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
-                        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
-                    </div>
-
-                    <div className="grid grid-cols-1">
-                        <div className="bg-white p-20 rounded-2xl border-1 border-gray-200">Content here</div>
-                    </div>
+                    { children }
                 </div>
             </div>
         </div>
-        );
+    );
   }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
 export default function Dashboard() {
   return (
     <>
+      <div className="grid grid-cols-4 gap-4 mb-5">
+        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
+        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
+        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
+        <div className="bg-white p-10 rounded-2xl border-1 border-gray-200">bg-white p-10 rounded-2xl border-1 border-gray-200</div>
+      </div>
+      <div className="grid grid-cols-1">
+          <div className="bg-white p-20 rounded-2xl border-1 border-gray-200">Content here</div>
+      </div>
     </>
   );
 }
